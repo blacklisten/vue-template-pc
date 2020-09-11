ed3969686d7549b0f20d822e7863638c99431223
wangjinjie <jinjie.wang@visionacademy.cn>
2020-09-11 19:37:13 +0800

    移出prompts注释

--- prompts.js
+++ prompts.js
@@ -4,47 +4,5 @@ module.exports = [
     type: 'checkbox',
     message: '请选择需要的依赖',
     choices: ['vuex', 'savml', 'va-study-public-sdk', 'elementUI', 'wxTools', 'sentry', 'wxui']
-  },
-  // {
-  //   name: "vuex",
-  //   type: "checkbox",
-  //   message: "是否需要安装vuex",
-  //   choices: []
-  // },
-  // {
-  //   name: "savml",
-  //   type: "checkbox",
-  //   message: "是否需要安装savml",
-  //   choices: []
-  // },
-  // {
-  //   name: 'va-study-public-sdk',
-  //   type: "checkbox",
-  //   message: "是否需要安装公共登陆sdk",
-  //   choices: []
-  // },
-  // {
-  //   name: "elementUI",
-  //   type: "checkbox",
-  //   message: "是否需要安装element-ui",
-  //   choices: []
-  // },
-  // {
-  //   name: "wxTools",
-  //   type: "checkbox",
-  //   message: "是否需要安装wx-tools",
-  //   choices: []
-  // },
-  // {
-  //   name: "sentry",
-  //   type: "checkbox",
-  //   message: "是否需要集成sentry",
-  //   choices: []
-  // },
-  // {
-  //   name: "wxui",
-  //   type: "checkbox",
-  //   message: "wxui",
-  //   choices: []
-  // }
+  }
 ]
